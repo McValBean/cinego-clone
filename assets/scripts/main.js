be52cf308df3dee4f95b662706ffd6bb738e0d66
mcvalbean <mcvalbean@gmail.com>
2024-12-20 14:18:25 +0000

    add homepage top-rated section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,37 @@
 console.log("we did it");
- let NewArray= [];
- if (condition) {
-    
- }
+
+const homeHeaderBackground = document.getElementsByClassName("home-header");
+
+const homeMovieList = document.getElementById("home-top-movies");
+const homeTVShowsList = document.getElementById("home-top-series");
+
+document.getElementById("home-top-movies").style.backgroundColor = "aqua";
+ document.getElementById("home-top-movies").style.color = "black";
+ document.getElementById("home-top-series").style.backgroundColor ="black";
+ document.getElementById("home-top-series").style.color="white";
+document.getElementById("series-section").style.display = "none";
+document.getElementById("movies-section").style.display= "flex";
+
+homeMovieList.onclick = function (){
+ document.getElementById("home-top-movies").style.backgroundColor = "aqua";
+ document.getElementById("home-top-movies").style.color = "black";
+ document.getElementById("home-top-series").style.backgroundColor ="black";
+ document.getElementById("home-top-series").style.color="white";
+document.getElementById("series-section").style.display = "none";
+document.getElementById("movies-section").style.display= "flex";}
+
+homeTVShowsList.onclick= function() {
+document.getElementById("home-top-series").style.backgroundColor="aqua";
+document.getElementById("home-top-movies").style.backgroundColor ="black";
+document.getElementById("home-top-series").style.color ="black";
+document.getElementById("home-top-movies").style.color ="white";
+document.getElementById("series-section").style.display = "flex";
+document.getElementById("movies-section").style.display= "none";}
+
+
+
+
+
+
+
 
