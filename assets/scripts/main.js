285f81ad839fa922b900816448594ebbd7ceb933
mcvalbean <mcvalbean@gmail.com>
2025-01-08 17:26:46 +0000

    add homepage trending list

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,62 @@
-console.log("we did it");
 
+
+//Below this is for the trending section of the homepage to toggle between movies and series
+
+const homeTrendingMovies = document.getElementById("home-trending-movies");
+const homeTrendingSeries = document.getElementById("home-trending-series");
+
+
+
+document.getElementById("home-trending-movies").style.backgroundColor = "aqua";
+console.log("1");
+document.getElementById("home-trending-movies").style.color = "black";
+console.log("2")
+document.getElementById("home-trending-series").style.backgroundColor ="black";
+console.log("3")
+document.getElementById("home-trending-series").style.color="white";
+console.log("4");
+document.getElementById("trending-series-list").style.display = "none";
+console.log("5");
+document.getElementById("trending-movies-list").style.display= "flex";
+console.log("6");
+
+homeTrendingMovies.onclick = function(){
+document.getElementById("home-trending-movies").style.backgroundColor = "aqua";
+document.getElementById("home-trending-movies").style.color = "black";
+document.getElementById("home-trending-series").style.backgroundColor ="black";
+document.getElementById("home-trending-series").style.color="white";
+document.getElementById("trending-series-list").style.display = "none";
+document.getElementById("trending-movies-list").style.display= "flex";
+}
+
+homeTrendingSeries.onclick = function(){
+document.getElementById("home-trending-series").style.backgroundColor="aqua";
+console.log(10);
+document.getElementById("home-trending-movies").style.backgroundColor ="black";
+console.log(20);
+document.getElementById("home-trending-series").style.color ="black";
+console.log(30);
+document.getElementById("home-trending-movies").style.color ="white";
+console.log(40);
+document.getElementById("trending-series-list").style.display = "flex";
+console.log(50);
+document.getElementById("trending-series-list").style.flexDirection = "column";
+console.log(60);
+document.getElementById("trending-movies-list").style.display= "none";
+console.log(70);
+}
+
+
+
+
+//below is the code for the top-rated section default layout //
 const homeMovieList = document.getElementById("home-top-movies");
 const homeTVShowsList = document.getElementById("home-top-series");
 
 document.getElementById("home-top-movies").style.backgroundColor = "aqua";
- document.getElementById("home-top-movies").style.color = "black";
- document.getElementById("home-top-series").style.backgroundColor ="black";
- document.getElementById("home-top-series").style.color="white";
+document.getElementById("home-top-movies").style.color = "black";
+document.getElementById("home-top-series").style.backgroundColor ="black";
+document.getElementById("home-top-series").style.color="white";
 document.getElementById("series-section").style.display = "none";
 document.getElementById("movies-section").style.display= "flex";
 
