f45e287e295c124c51d52a741dd37bf8509a8fe7
mcvalbean <mcvalbean@gmail.com>
2025-02-04 17:05:42 +0000

    add top imdb content

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-//this code is for the dialog box for the login//
+//this code is for the dialog box for the login
 const login= document.getElementById("openDialog");
 const loginBox = document.getElementById("dialogBox");
 const closeLogin = document.getElementById("closeDIalog");
@@ -41,16 +41,16 @@ registerLink.onclick = function() {
 
 //Below this is for the trending section of the homepage to toggle between movies and series//
 
-let homeTrendingMoviesLink = document.getElementById("home-trending-movies");
-let homeTrendingSeriesLink = document.getElementById("home-trending-series");
+const homeTrendingMoviesLink = document.getElementById("home-trending-movies");
+const homeTrendingSeriesLink = document.getElementById("home-trending-series");
 
 
-document.getElementById("home-trending-movies").style.backgroundColor = "aqua";
-document.getElementById("home-trending-movies").style.color= "black";
-document.getElementById("home-trending-series").style.backgroundColor ="black";
-document.getElementById("home-trending-series").style.color="white";
-document.getElementById("trending-movies-list").style.display="flex";
+homeTrendingMoviesLink.style.backgroundColor ="aqua";
+homeTrendingMoviesLink.style.color ="black";
+homeTrendingSeriesLink.style.backgroundColor ="black";
+homeTrendingSeriesLink.style.color ="white";
 document.getElementById("trending-series-list").style.display ="none";
+document.getElementById("trending-movies-list").style.display="flex";
 
 
 homeTrendingMoviesLink.onclick = function (){
@@ -64,8 +64,8 @@ document.getElementById("trending-movies-list").style.display= "flex";
 
 homeTrendingSeriesLink.onclick = function (){
 document.getElementById("home-trending-series").style.backgroundColor="aqua";
-document.getElementById("home-trending-movies").style.backgroundColor ="black";
 document.getElementById("home-trending-series").style.color ="black";
+document.getElementById("home-trending-movies").style.backgroundColor ="black";
 document.getElementById("home-trending-movies").style.color ="white";
 document.getElementById("trending-series-list").style.display ="flex";
 document.getElementById("trending-series-list").style.flexDirection ="column";
@@ -94,7 +94,7 @@ homeMovieList.onclick = function(){
 document.getElementById("series-section").style.display = "none";
 document.getElementById("movies-section").style.display= "flex";}
 
-homeTVShowsList.onclick= function(){
+homeTVShowsList.onclick = function(){
 home.style.backgroundColor="aqua";
 document.getElementById("home-top-movies").style.backgroundColor ="black";
 document.getElementById("home-top-series").style.color ="black";
@@ -102,34 +102,6 @@ document.getElementById("home-top-movies").style.color ="white";
 document.getElementById("series-section").style.display = "flex";
 document.getElementById("series-section").style.flexDirection = "column";
 document.getElementById("movies-section").style.display= "none";}
-
-
-
-// this is a function to change the background of the home header to match the content being shown on the home header//
-
-let homeHeaderBackground = document.getElementById("homeHeaderBackground");
-let latestImage = document.getElementById("latestImage");
-let latestContentInfo = document.getElementById("")
-let latestContent = [];
-let previous = document.getElementById("previous");
-let next = document.getElementById("next");
-
-previous.onclick = function (){
-    document.getElementById("trending").Child();
-}
-const toggle = document.getElementById("toggle");
-const lightBackground = document.getElementsByClassName("").backgroundColor="rgb(70,70,70)";
-
-
-toggle.onclick = function (){
-    if(lightBackground=true)
-         {
-                document.getElementsByClassName("series-episode").style.backgroundColor="black"
-          }
-           else{
-        lightBackground=true;
-    }
-    }
 
 
 
